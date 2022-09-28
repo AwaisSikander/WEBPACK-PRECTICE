b7fff89563bd4b598c276df9c3d5f392b3083ac3
Awais Sikander <AwaisRegbits>
2022-09-28 22:55:27 +0500

    Loader Prectice

---
+++ 3. Css Loaders/src/clearButton.js
@@ -0,0 +1,8 @@
+// import style from './clearButton.css'
+const el = document.createElement('button')
+el.innerHTML = 'Clear'
+el.classList.add(['button'])
+el.onclick = function(){
+    alert('Clicked')
+}
+document.body.appendChild(el)
